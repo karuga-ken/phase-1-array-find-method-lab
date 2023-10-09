ea39331dfae1a06e7b3f2febee20295a2cc4460e
Karuga Mbugua <karugambugua@icloud.com>
2023-10-09 22:35:51 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+const record = [
+    {
+        year: "2015",
+        result: 'W',
+    },
+    {
+        year: "2014",
+        result: 'N/A',
+    },
+    {
+        year:"2013",
+        result:"L"
+    }
+]
+let superbowlWin = record => {
+    let result = record.find(record => record.result === "W")
+}
+console.log(result);
+
